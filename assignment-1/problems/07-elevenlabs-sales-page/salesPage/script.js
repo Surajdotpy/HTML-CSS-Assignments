8ee715afee1c42e9e530091c8c4fb3a8cbe657ba
Suraj Kumar <itssurajkumar3@gmail.com>
2026-01-21 18:40:27 +0530

    add eleven labs sales page

---
+++ assignment-1/problems/07-elevenlabs-sales-page/salesPage/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded" , () =>{
+    const line1 = document.querySelector(".line1");
+    line1.style.backgroundColor = "black";
+    line1.style.transform = "scaleX(1)";
+});
